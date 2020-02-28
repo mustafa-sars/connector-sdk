ee3d57af1d2b5c16fec20255857e53c7743d31c8
Will Fleming <will@flemi.ng>
2020-02-28 15:47:51 -0500

    Export stream & state related things from index

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export {
 } from "./Client"
 export { Logger } from "./Logger"
 export { RecordProducer, RecordProducerFacade } from "./RecordProducer"
+export { Stream } from "./Stream"
+export { State, StateManager } from "./StateManager"
